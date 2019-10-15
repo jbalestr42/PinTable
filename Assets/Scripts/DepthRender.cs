d0cb4226021d43d03c43456c2960e702354c1166
Julien Balestrieri <julien.balestrieri@gmail.com>
2019-10-15 23:55:48 +0200

    Add new example with a 3D asset

---
+++ Assets/Scripts/DepthRender.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthRender : MonoBehaviour
+{
+    public Material _material;
+
+    void Start()
+    {
+       GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, _material);
+    }
+}
---
+++ Assets/Scripts/UpdateUnityChanAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This code mainly come from the UnityChan asset
+public class UpdateUnityChanAnimation : MonoBehaviour
+{
+	public float _interval = 2f;
+
+	private Animator _animator;
+	private AnimatorStateInfo _currentState;
+	private AnimatorStateInfo _previousState;
+
+    void Start()
+    {
+		_animator = GetComponent<Animator> ();
+		_currentState = _animator.GetCurrentAnimatorStateInfo(0);
+		_previousState = _currentState;
+		StartCoroutine(RandomChange());
+    }
+
+    void Update()
+    {
+		if (_animator.GetBool ("Next"))
+        {
+			_currentState = _animator.GetCurrentAnimatorStateInfo(0);
+			if (_previousState.fullPathHash != _currentState.fullPathHash)
+            {
+				_animator.SetBool ("Next", false);
+				_previousState = _currentState;				
+			}
+		}
+    }
+    
+	IEnumerator RandomChange()
+	{
+		while (true)
+        {
+			_animator.SetBool("Next", true);
+			yield return new WaitForSeconds (_interval);
+		}
+	}
+}
